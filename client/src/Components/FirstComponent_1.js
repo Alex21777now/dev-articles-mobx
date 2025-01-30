7fb02f58a5946aadfd753c23232bc40f7f78b1a7
Alex21777now <banna707@i.ua>
2025-01-30 19:42:01 +0200

    sweeping Component1 and Page1

--- client/src/Components/FirstComponent_1.js
+++ client/src/Components/FirstComponent_1.js
@@ -9,8 +9,6 @@ import Form from "./Form.js";
 import KursVse from "./KursVse";
 
 
-
-
 export default class FirstComponent_1 extends Component {
 
 
@@ -72,7 +70,7 @@ console.log(this.state);
  
           <div className="flex-child magenta">
     
-          
+{/*          
 
       <Form kursMethod1={this.gettingKurs1}
 
@@ -124,8 +122,35 @@ console.log(this.state);
    />
  }
      </>
+*/}
+  
+  Fortune
+Elon Musk claims full self driving is so advanced Tesla owners are turning it off and steering with their knees to check text messages
+Amanda Gerut
+Updated Thu, January 30, 2025 at 11:59 AM GMT+2·3 min read
+Tesla CEO Elon Musk told investors and analysts the electric vehicle maker’s investments in autonomous vehicles and robots may have set it on a path to becoming the most valuable company in the world.
 
-       
+During a call with investors and analysts on Wednesday, Tesla CEO Elon Musk said the company is on course to reap immense gains in the coming years due to its development of autonomy. In fact, he said Tesla’s full self driving (FSD) feature has advanced so profoundly in its development that drivers are turning the feature off so they don’t get hit with a series of annoying beeps for removing their hands from the steering wheel.
+
+“Right now, there is this perverse situation where people actually go to manual driving to check their text messages so the computer doesn’t yell at them,” said Musk. Drivers turn off autopilot, he claimed, and steer with their knees so they can read emails and texts. This is “significantly less safe” than if drivers could take their hands off the steering wheel, he noted. Musk commented in response to an analyst who complained that autopilot was “super boring” because he didn’t need to intervene. The analyst asked Musk if Tesla was considering offering unsupervised driving that allowed a user five seconds or so to handle their phones.
+
+Musk’s leadership at Tesla has seen numerous investigations and recalls of the cars in recent years. In contrast, the EU’s regulatory approach is slower and FSD still isn’t permitted overseas.
+Musk cautioned that Tesla is taking it slow with FSD and will roll out an unsupervised autonomous fleet in Austin, Tex. in June. He compared it to dipping a toe in the water, followed by a few toes, followed by a foot, before diving in as a safety precaution.
+
+Musk made his comments during Tesla’s fourth quarter question-and-answer call with financial analysts after the market closed on Wednesday. It was Musk’s first exchange with analysts since President Trump’s election win in November. He avoided answering questions about his proximity to Trump, despite significant shareholder interest in the topic.
+
+Prior to the call, the most up-voted question about Musk posed by a retail shareholder on Tesla’s online platform for Q&A was: “How is having a position in the White House going to affect the time spent with Tesla?” Runners up were variations on the same theme. Shareholders wanted to know whether Musk was dedicating enough time to Tesla, whether sales were being lost due to Musk’s political activities, and clarity on the hand gestures Musk made during President Trump’s inauguration.
+
+The questions were not asked during the Q&A.
+
+For the full year 2024, Tesla’s revenues were up 1% to $98 billion while its vehicle deliveries fell 1% year-over-year to 1.8 million. Operating expenses rose 18% for the year, largely because of significant spending on artificial intelligence and research and development efforts.
+
+Musk said the investments in 2024 were critical to paving the way for manufacturing products that will feature AI and robotics. Yet, he also predicted the investment will bear fruit in the future that could see Tesla become the most valuable company in the world “by far,” said Musk.
+
+He said critics of Tesla’s plans for an autonomous future have called him the “boy who cried wolf” in the past but now, Tesla has a “self-driving wolf.” He claimed prior versions of FSD, which he compared to a toddler who has begun to mature, have been developed more thoroughly.
+
+This story was originally featured on Fortune.com
+
 </div>
 
     </div>
